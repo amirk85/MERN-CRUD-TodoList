f28cfe3057c64d8459029cee0b2b2899815567c7
Amir <khan.ayyub@happiestminds.com>
2022-05-13 18:51:54 +0530

    .

---
+++ client/src/app/App.js
@@ -0,0 +1,14 @@
+import Todos from "../components/Todos";
+import "./App.css";
+
+function App() {
+  return (
+    <div
+      style={{ display: "flex", justifyContent: "center", marginTop: "2rem" }}
+    >
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
